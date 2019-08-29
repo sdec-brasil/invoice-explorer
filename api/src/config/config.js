0860374766d43c0f1cffd772de0c38d85559fe6c
David Beyda <dbeyda@globo.com>
2019-08-29 16:25:59 -0300

    Getting redis config from env

--- api/src/config/config.js
+++ api/src/config/config.js
@@ -19,9 +19,8 @@ export const limitSettings = {
     get: 100,
   },
 };
-
 export const configRedis = {
-  host: '127.0.0.1',
-  port: 6379,
+  host: process.env.REDIS_HOST ? process.env.REDIS_HOST : '127.0.0.1',
+  port: process.env.REDIS_PORT ? process.env.REDIS_PORT : 6379,
   db: 0,
 };
